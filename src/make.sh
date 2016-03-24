05cc1b1647b79230ab758518e27dfbf7e2e631d3
omelancon <ol.melancon@gmail.com>
2016-03-23 20:32:00 -0400

    compiling main.ml

--- src/make.sh
+++ src/make.sh
@@ -9,11 +9,11 @@ echo -e "  \e[33m=====================\e[0m"
 
 
 echo -e "\e[1m\e[33m- Menhir : parser.mly -> parser.mli-> parser.ml \e[0m"
-menhir parser.mly 2> menhir_output.txt
-echo "  See menhir_output.txt for errors and warnings"
+menhir --infer parser.mly 2> menhir_output.txt
+echo "See menhir_output.txt for errors and warnings"
 
 echo -e "\e[1m\e[33m- OCamllex : lex.mll -> lex.ml\e[0m"
 ocamllex lex.mll
 
-echo -e "\e[1m\e[33m- OCamlBuild -> main.ml\e[0m"
-ocamlbuild -no-hygiene main.native
+echo -e "\e[1m\e[33m- Corebuild -> main.ml\e[0m"
+corebuild -no-hygiene lex.ml parser.ml weeder.ml prettyPrint.ml main.ml
